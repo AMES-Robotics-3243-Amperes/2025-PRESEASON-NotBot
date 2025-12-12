2995d2a9a499df6fbbdb2cbcb71be55048213e68
davidmartinez-ross <david.martinezross@gmail.com>
2025-12-11 17:07:17 -0700

    shooter+index start

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,7 @@
+public class shooterSubsystem extends SubsystemBase{
+    Sparkmax motor1 = new Sparkmax(0,MotorType.kBrushless);
+    public void setSpeed(double speed) {
+        return runEnd
+    }
+    
+}
